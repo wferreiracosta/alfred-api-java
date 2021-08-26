dcd8f694392b1743a035e5b683931f04efeac444
Wesley Costa <wesferrcosta@gmail.com>
2021-08-25 22:51:02 -0300

    first commit

---
+++ src/main/java/br/com/wferreiracosta/alfred/AlfredApplication.java
@@ -0,0 +1,13 @@
+package br.com.wferreiracosta.alfred;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlfredApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlfredApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/wferreiracosta/alfred/AlfredApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.wferreiracosta.alfred;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlfredApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
