0b0a6d253d743f8e0f235dbaefceb54d8681ac9d
Wesley Costa <wesferrcosta@gmail.com>
2021-09-04 19:24:19 -0300

    feat: Serviço de pedido

---
+++ src/main/java/br/com/wferreiracosta/alfred/services/PedidoService.java
@@ -0,0 +1,9 @@
+package br.com.wferreiracosta.alfred.services;
+
+import br.com.wferreiracosta.alfred.models.Pedido;
+
+public interface PedidoService {
+
+    public Pedido save(Pedido pedido);
+
+}
---
+++ src/main/java/br/com/wferreiracosta/alfred/services/impl/PedidoServiceImpl.java
@@ -0,0 +1,22 @@
+package br.com.wferreiracosta.alfred.services.impl;
+
+import br.com.wferreiracosta.alfred.models.Pedido;
+import br.com.wferreiracosta.alfred.repositories.PedidoRepository;
+import br.com.wferreiracosta.alfred.services.PedidoService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PedidoServiceImpl implements PedidoService {
+
+    private final PedidoRepository repository;
+
+    public PedidoServiceImpl(PedidoRepository repository) {
+        this.repository = repository;
+    }
+
+    @Override
+    public Pedido save(Pedido pedido) {
+        return this.repository.save(pedido);
+    }
+
+}
