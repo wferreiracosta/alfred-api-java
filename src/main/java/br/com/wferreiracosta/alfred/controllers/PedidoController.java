05048221fd8af67dc2bb14cff58aca18b42927e5
Wesley Costa <wesferrcosta@gmail.com>
2022-01-08 19:45:40 -0300

    feat: Criado endpoint para criar pedido

--- src/main/java/br/com/wferreiracosta/alfred/controllers/PedidoController.java
+++ src/main/java/br/com/wferreiracosta/alfred/controllers/PedidoController.java
@@ -5,7 +5,12 @@ import br.com.wferreiracosta.alfred.models.Pedido;
 import br.com.wferreiracosta.alfred.services.PedidoService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import javax.validation.Valid;
+import java.net.URI;
 
 @Slf4j
 @RestController
@@ -24,6 +29,14 @@ public class PedidoController {
         log.info("[GET] Obtendo Pedido por id: ID = "+id);
         return this.pedidoService.findById(id)
                 .orElseThrow(() -> new ObjectNotFoundException("Pedido não encontrado! Id: " + id));
+    }
+
+    @RequestMapping(method = RequestMethod.POST)
+    public ResponseEntity<Void> insert(@Valid @RequestBody Pedido obj) {
+        log.info("[POST] Inserido Pedido : "+obj);
+        obj = this.pedidoService.insert(obj);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+        return ResponseEntity.created(uri).build();
     }
 
 }
--- src/main/java/br/com/wferreiracosta/alfred/services/impl/PedidoServiceImpl.java
+++ src/main/java/br/com/wferreiracosta/alfred/services/impl/PedidoServiceImpl.java
@@ -1,29 +1,75 @@
 package br.com.wferreiracosta.alfred.services.impl;
 
+import br.com.wferreiracosta.alfred.models.ItemPedido;
+import br.com.wferreiracosta.alfred.models.PagamentoComBoleto;
 import br.com.wferreiracosta.alfred.models.Pedido;
+import br.com.wferreiracosta.alfred.models.enums.EstadoPagamento;
+import br.com.wferreiracosta.alfred.repositories.ClienteRepository;
+import br.com.wferreiracosta.alfred.repositories.ItemPedidoRepository;
+import br.com.wferreiracosta.alfred.repositories.PagamentoRepository;
 import br.com.wferreiracosta.alfred.repositories.PedidoRepository;
+import br.com.wferreiracosta.alfred.services.BoletoService;
 import br.com.wferreiracosta.alfred.services.PedidoService;
+import br.com.wferreiracosta.alfred.services.ProdutoService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDateTime;
 import java.util.Optional;
 
 @Service
 public class PedidoServiceImpl implements PedidoService {
 
-    private final PedidoRepository repository;
+    @Autowired
+    private PagamentoRepository pagamentoRepository;
+    @Autowired
+    private ClienteRepository clienteRepository;
+    @Autowired
+    private ItemPedidoRepository itemPedidoRepository;
+    @Autowired
+    private ProdutoService produtoService;
+    @Autowired
+    private BoletoService boletoService;
+
+    private final PedidoRepository pedidoRepository;
 
     public PedidoServiceImpl(PedidoRepository repository) {
-        this.repository = repository;
+        this.pedidoRepository = repository;
     }
 
     @Override
     public Pedido save(Pedido pedido) {
-        return this.repository.save(pedido);
+        return this.pedidoRepository.save(pedido);
     }
 
     @Override
     public Optional<Pedido> findById(Integer id) {
-        return this.repository.findById(id);
+        return this.pedidoRepository.findById(id);
+    }
+
+    @Transactional
+    @Override
+    public Pedido insert(Pedido obj) {
+        obj.setId(null);
+        obj.setInstante(LocalDateTime.now());
+        obj.setCliente(clienteRepository.findById(obj.getCliente().getId()).get());
+        obj.getPagamento().setEstado(EstadoPagamento.PENDENTE);
+        obj.getPagamento().setPedido(obj);
+        if (obj.getPagamento() instanceof PagamentoComBoleto) {
+            PagamentoComBoleto pagto = (PagamentoComBoleto) obj.getPagamento();
+            boletoService.preencherPagamentoComBoleto(pagto, LocalDateTime.now());
+        }
+        obj = pedidoRepository.save(obj);
+        pagamentoRepository.save(obj.getPagamento());
+        for (ItemPedido ip : obj.getItens()) {
+            ip.setDesconto(0.0);
+            ip.setProduto(produtoService.findById(ip.getProduto().getId()).get());
+            ip.setPreco(produtoService.findById(ip.getProduto().getId()).get().getPreco());
+            ip.setPedido(obj);
+        }
+        itemPedidoRepository.saveAll(obj.getItens());
+        return obj;
     }
 
 }
