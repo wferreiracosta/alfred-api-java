57de072b6a86319b8fec9dba638fe1cdae231766
Wesley Costa <wesferrcosta@gmail.com>
2022-01-08 22:32:12 -0300

    fix: Modificado o Devconfig para user o strategy do properties

--- src/main/java/br/com/wferreiracosta/alfred/config/DevConfig.java
+++ src/main/java/br/com/wferreiracosta/alfred/config/DevConfig.java
@@ -2,6 +2,7 @@ package br.com.wferreiracosta.alfred.config;
 
 import br.com.wferreiracosta.alfred.services.DataBaseService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -13,8 +14,14 @@ public class DevConfig {
     @Autowired
     private DataBaseService dbService;
 
+    @Value("${spring.jpa.hibernate.ddl-auto")
+    private String strategy;
+
     @Bean
     public boolean instantiateDatabase() {
+        if(!"create".equals(strategy)){
+            return false;
+        }
         dbService.instantiateTestDatabase();
         return true;
     }
